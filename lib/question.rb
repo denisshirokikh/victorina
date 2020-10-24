4057a1e47423cd7bb3acddc20c611081360832e4
Denis Shirokikh <denisshirokikh@gmail.com>
2020-10-23 17:36:44 -0700

    added class answer, made refactoring

--- lib/question.rb
+++ lib/question.rb
@@ -1,29 +1,9 @@
 class Question
-  def initialize(text, answer, points, variants=[], waiting_time)
+  attr_reader :text, :points, :wait_time, :answers
+  def initialize(text, points, wait_time, answers = [] )
     @text = text
-    @answer = answer
     @points = points.to_i
-    @variants = variants
-    @waiting_time = waiting_time.to_i
-  end
-
-  def question_text
-    @text
-  end
-
-  def correct_answer
-    @answer
-  end
-
-  def score
-    @points
-  end
-
-  def due_time
-    @waiting_time
-  end
-
-  def list_variants
-    @variants
+    @wait_time = wait_time.to_i
+    @answers = answers
   end
 end
--- main.rb
+++ main.rb
@@ -1,4 +1,5 @@
 require_relative 'lib/question'
+require_relative 'lib/answer'
 require 'rexml/document'
 
 if (Gem.win_platform?)
@@ -14,19 +15,17 @@ file = File.new("#{__dir__}/data/data.xml", encoding: 'UTF-8')
 doc = REXML::Document.new(file)
 file.close
 
-game_set = []
-
-doc.elements.each('questions/question') do |item|
-  right_answer = ''
-  item.elements.each('./variants/variant/') { |variant| right_answer = variant.text if variant.attributes['right'] }
-  game_set << Question.new(
-    item.elements['text'].text,
-    right_answer,
-    item.attributes['points'],
-    item.elements['variants'].elements.map(&:text),
-    item.attributes['minutes']
-    )
-end
+game_set =
+    doc.elements['questions'].elements.map do |item|
+      right_answer = ''
+      item.elements.each('./variants/variant/') { |variant| right_answer = variant.text if variant.attributes['right'] }
+      Question.new(
+          item.elements['text'].text,
+          item.attributes['points'],
+          item.attributes['minutes'],
+          Answer.new(right_answer, item.elements['variants'].elements.map(&:text))
+      )
+    end
 
 puts "Let's play..please answer my questions:"
 
@@ -35,19 +34,22 @@ score = 0
 
 game_set.sample(5).each do |try|
   time_start = Time.now
-  puts "#{try.question_text} (You have 5 sec for answer)"
-  "#{try.list_variants.each {|variant| puts variant}}"
-  user_input = STDIN.gets.chomp
-  if user_input == try.correct_answer && time_answer - time_start <= try.due_time
-    puts "That's right! You got (#{try.score} points)"
+  puts "#{try.text} (You have 5 sec for answer)"
+  "#{try.answers.list.each {|variant| puts variant}}"
+  user_input = STDIN.gets.to_i
+  time_answer = Time.now
+  if user_input == try.answers.right_answer.to_i &&
+      time_answer - time_start <= try.wait_time
+    puts "That's right! You got (#{try.points} points)"
     counter += 1
-    score += try.score
-  elsif Time.now > time_start + try.due_time
-    puts "Your time is up! Right answer: #{try.correct_answer}"
+    score += try.points
+  elsif Time.now > time_start + try.wait_time
+    puts "Your time is up! Right answer: #{try.answers.right_answer}"
   else
-    puts "Wrong. Right answer: #{try.correct_answer}"
+    puts "Wrong. Right answer: #{try.answers.right_answer}"
   end
 end
 
 puts "Right answers: #{counter} from 5"
 puts "You got #{score} points"
+
